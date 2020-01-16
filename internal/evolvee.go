49156c2307b0c43e520ef365f0d2d67de1890406
David Federspiel <david@federnet.com>
2020-01-16 00:15:45 -0500

    added evolvee class

--- internal/evolvee.go
+++ internal/evolvee.go
@@ -1,1 +1,25 @@
-package internal
+package evolvee
+
+import "fmt"
+
+type evolvee struct {
+	 Classification string
+	 Id int
+	 X int
+	 Y int
+	 Maturity int
+}
+
+func xy(e evolvee) string {
+	return fmt.Sprintf("%d,%d", e.X, e.Y)
+}
+
+func New(classification string, id, x, y int) *evolvee {
+	e := new(evolvee)
+	e.Classification = classification
+	e.Id = id
+	e.X = x
+	e.Y = y
+	e.Maturity = 1
+	return e
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package evolver
+package main
+
+import (
+	evolvee "evolver/internal"
+	"fmt"
+)
+
+func main(){
+	e := evolvee.New("manga", 123, 0,0)
+	fmt.Println(e.Classification)
+
+}
